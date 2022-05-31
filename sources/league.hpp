0795ac379d8531355b72e3a547bd016635da178b
Yehudit-Brickner <nech.diti@yahoo.com>
2022-06-01 01:26:54 +0300

    add

--- sources/league.hpp
+++ sources/league.hpp
@@ -24,8 +24,8 @@ class league {
     // }
     league(vector<team*> t){
         this->_teams=t;
-        vector<vector<vector<team*>>> x(t.size(), vector<vector<team*>>(t.size(), vector<team*>(t.size())));
-        this->overall=x;
+        // vector<vector<vector<team*>>> x(t.size(), vector<vector<team*>>(t.size(), vector<team*>(t.size())));
+        // this->overall=x;
         
     }
 
@@ -34,14 +34,14 @@ class league {
         for ( unsigned long i=1; i<this->_teams.size();i++){
             for (unsigned long j=0; j<this->_teams.size();j++){
                 game g(this->_teams[j],this->_teams[(i+j)%this->_teams.size()]);
-                if(g._score_home>=g._score_away){
-                    overall[j][i][0] = this->_teams[j] ; //winner
-                    overall[j][i][0]  = this->_teams[(i+j)%this->_teams.size()] ; //loser
-                }
-                else{
-                    overall[j][i][0] = this->_teams[(i+j)%this->_teams.size()] ; //winner
-                    overall[j][i][0]  = this->_teams[j] ; //loser 
-                }
+                // if(g._score_home>=g._score_away){
+                //     overall[j][i][0] = this->_teams[j] ; //winner
+                //     overall[j][i][0]  = this->_teams[(i+j)%this->_teams.size()] ; //loser
+                // }
+                // else{
+                //     overall[j][i][0] = this->_teams[(i+j)%this->_teams.size()] ; //winner
+                //     overall[j][i][0]  = this->_teams[j] ; //loser 
+                // }
             }
 
         }
